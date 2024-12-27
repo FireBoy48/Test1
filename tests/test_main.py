66e2f2e6d83447c2812d4c2693f0501b970af6fe
Kirill Kosmodemyanskii <gert48750@mail.ru>
2024-12-27 23:40:47 +0300

    10.2.7 done

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,10 +17,18 @@ def test_calc_log():
         calculate_log(2, 0)
 
 
-def test_reverse_string_numbers(numbers):
-    assert reverse_string('123') == numbers
+@pytest.mark.parametrize('value, expected',[
+                         ('123', '321'),
+                         ('hello', 'olleh'),
+                         ('world', 'dlrow')
+                         ])
+def test_reverse_string(value, expected):
+    assert reverse_string(value) == expected
 
-
-def test_reverse_string_letters(letters):
-    assert reverse_string('hello') == letters
+# def test_reverse_string_numbers(numbers):
+#     assert reverse_string('123') == numbers
+#
+#
+# def test_reverse_string_letters(letters):
+#     assert reverse_string('hello') == letters
 
